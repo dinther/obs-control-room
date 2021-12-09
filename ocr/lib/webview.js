534ba815426e6ec84a7e2bc3e3aed91279eed15c
Paul van Dinther <vandinther@gmail.com>
2021-12-09 14:17:42 -0800

    Delete ocr directory

--- ocr/lib/webview.js
+++
@@ -1,6 +0,0 @@
-if (window.chrome.webview !== undefined){
-    function sendHotKey(application, hotkey){
-        window.chrome.webview.postMessage({command:'sendHotkey', application: 'application', hotkey: hotkey});
-    }
-    window.chrome.webview
-}
